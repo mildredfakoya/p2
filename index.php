e92ee75df9c6c504382f7e237dfea452f0f63a9b
Mildred Fakoya <luvlyayee@gmail.com>
2017-09-17 10:22:58 -0400

    created an index file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang ="en">
+<head>
+<title> Registration Form </title>
+<meta charset ="utf-8">
+<meta name ="viewport" content ="width=device-width, initial-scale =1.0">
+</head>
+<body>
+  <h1>Create a New Screening</h1>
+</body>
+
+</html>
